aea20c63a1efe9c3d4cb39098e5dddc16203cda6
Chris Daffron <cdaffron@vols.utk.edu>
2015-01-29 12:56:24 -0500

    Continued lab1 with sine wave and polling parts

---
+++ lab1/loop_poll/Debug/loop_poll.pp
@@ -0,0 +1,35 @@
+# FIXED
+
+loop_poll.obj: ../loop_poll.c
+loop_poll.obj: C:/ti/CCStudio/c6000/dsk6713/include/DSK6713_AIC23.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_chip.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_stdinc.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_stdinchal.h
+loop_poll.obj: C:/ti/C6xCSL/include/tistdtypes.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_chiphal.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_emifhal.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_chip.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_irq.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_irqhal.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_timer.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_timerhal.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_mcbsp.h
+loop_poll.obj: C:/ti/C6xCSL/include/csl_mcbsphal.h
+
+../loop_poll.c: 
+C:/ti/CCStudio/c6000/dsk6713/include/DSK6713_AIC23.h: 
+C:/ti/C6xCSL/include/csl.h: 
+C:/ti/C6xCSL/include/csl_chip.h: 
+C:/ti/C6xCSL/include/csl_stdinc.h: 
+C:/ti/C6xCSL/include/csl_stdinchal.h: 
+C:/ti/C6xCSL/include/tistdtypes.h: 
+C:/ti/C6xCSL/include/csl_chiphal.h: 
+C:/ti/C6xCSL/include/csl_emifhal.h: 
+C:/ti/C6xCSL/include/csl_chip.h: 
+C:/ti/C6xCSL/include/csl_irq.h: 
+C:/ti/C6xCSL/include/csl_irqhal.h: 
+C:/ti/C6xCSL/include/csl_timer.h: 
+C:/ti/C6xCSL/include/csl_timerhal.h: 
+C:/ti/C6xCSL/include/csl_mcbsp.h: 
+C:/ti/C6xCSL/include/csl_mcbsphal.h: 
--- lab1/sine8_LED/Sine8_LED.c
+++ lab1/sine8_LED/Sine8_LED.c
@@ -7,8 +7,12 @@ Uint32 fs = DSK6713_AIC23_FREQ_8KHZ;   //set sampling rate
 Uint16 inputsource=DSK6713_AIC23_INPUT_MIC;//select input
 #define LOOPLENGTH 8
 short loopindex = 0;         	      //table index
+short loopincr = 1;					  //loop increment value
 short gain = 10;			          //gain factor
+int timeCounter = 0;
+short alternate = 0;
 short sine_table[LOOPLENGTH]={0,707,1000,707,0,-707,-1000,-707}; //sine values
+short sine_table2[LOOPLENGTH*2]={0,383,707,924,1000,924,707,383,0,-383,-707,-924,-1000,-924,-707,-383};
 
 void main()
 {
@@ -17,12 +21,35 @@ void main()
   DSK6713_DIP_init();			      //init DIP from BSL
   while(1) 				              //infinite loop
   {
-    if(DSK6713_DIP_get(0)==0) 		  //=0 if DIP switch #0 pressed
+	if(DSK6713_DIP_get(3)==0)
+	{
+		DSK6713_LED_on(3);
+		DSK6713_LED_off(0);
+		timeCounter = 0;
+		alternate = 0;
+		while(timeCounter < 40000)
+		{
+			//if(alternate%2 == 0)
+			//{
+				output_left_sample(sine_table[loopindex++]*gain);
+			//}
+			alternate++;
+			if(loopindex >= LOOPLENGTH*2) loopindex = 0;
+			timeCounter++;
+		}
+		DSK6713_LED_off(3);
+	}
+	else if(DSK6713_DIP_get(0)==0) 		  //=0 if DIP switch #0 pressed
     {
-      DSK6713_LED_on();	   		      //turn LED #0 ON
-      output_left_sample(sine_table[loopindex++]*gain); //output sample
+	  DSK6713_LED_off(3);
+      DSK6713_LED_on(0);	   		      //turn LED #0 ON
+      output_left_sample(sine_table[loopindex+=loopincr]*gain); //output sample
       if (loopindex >= LOOPLENGTH) loopindex = 0; //reset table index
     }
-    else DSK6713_LED_off(0); 		  //turn LED off if not pressed
+    else
+    {
+    	DSK6713_LED_off(0); 		  //turn LED off if not pressed
+    	DSK6713_LED_off(3);
+    }
   }                                   //end of while(1) infinite loop
 }					                  //end of main
