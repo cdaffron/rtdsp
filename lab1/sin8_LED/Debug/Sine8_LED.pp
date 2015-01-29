be8813579f130a5679c7b001d970bb0c19c82a42
Chris Daffron <cdaffron@vols.utk.edu>
2015-01-28 20:44:11 -0500

    Started Lab 1

---
+++ lab1/sin8_LED/Debug/Sine8_LED.pp
@@ -0,0 +1,35 @@
+# FIXED
+
+Sine8_LED.obj: ../Sine8_LED.c
+Sine8_LED.obj: C:/ti/CCStudio/c6000/dsk6713/include/dsk6713_aic23.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_chip.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_stdinc.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_stdinchal.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/tistdtypes.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_chiphal.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_emifhal.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_chip.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_irq.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_irqhal.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_timer.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_timerhal.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_mcbsp.h
+Sine8_LED.obj: C:/ti/C6xCSL/include/csl_mcbsphal.h
+
+../Sine8_LED.c: 
+C:/ti/CCStudio/c6000/dsk6713/include/dsk6713_aic23.h: 
+C:/ti/C6xCSL/include/csl.h: 
+C:/ti/C6xCSL/include/csl_chip.h: 
+C:/ti/C6xCSL/include/csl_stdinc.h: 
+C:/ti/C6xCSL/include/csl_stdinchal.h: 
+C:/ti/C6xCSL/include/tistdtypes.h: 
+C:/ti/C6xCSL/include/csl_chiphal.h: 
+C:/ti/C6xCSL/include/csl_emifhal.h: 
+C:/ti/C6xCSL/include/csl_chip.h: 
+C:/ti/C6xCSL/include/csl_irq.h: 
+C:/ti/C6xCSL/include/csl_irqhal.h: 
+C:/ti/C6xCSL/include/csl_timer.h: 
+C:/ti/C6xCSL/include/csl_timerhal.h: 
+C:/ti/C6xCSL/include/csl_mcbsp.h: 
+C:/ti/C6xCSL/include/csl_mcbsphal.h: 
