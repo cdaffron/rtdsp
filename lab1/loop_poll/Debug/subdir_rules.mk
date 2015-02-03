2b5d96b6192790a236777ad18724b909c1dce83a
Chris Daffron <cdaffron@vols.utk.edu>
2015-02-03 10:08:43 -0500

    More files from lab 1

--- lab1/loop_poll/Debug/subdir_rules.mk
+++ lab1/loop_poll/Debug/subdir_rules.mk
@@ -6,21 +6,21 @@
 Vectors_poll.obj: ../Vectors_poll.asm $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: C6000 Compiler'
-	"c:/ti/ccsv6/tools/compiler/c6000_7.4.8/bin/cl6x" -mv6710 --abi=coffabi -g --include_path="C:/ti/C6xCSL/include" --include_path="C:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="c:/ti/ccsv6/tools/compiler/c6000_7.4.8/include" --include_path="/packages/ti/xdais" --include_path="/include" --include_path="c:/CCStudio_v3.1/MyProjects/Support" --include_path="C:/ti/CCStudio/c6000/dsk6713/include" --define="CHIP_6713" --display_error_number --diag_wrap=off --mem_model:data=far --preproc_with_compile --preproc_dependency="Vectors_poll.pp" $(GEN_OPTS__FLAG) "$<"
+	"c:/ti/ccsv6/tools/compiler/c6000_7.4.12/bin/cl6x" -mv6710 --abi=coffabi -g --include_path="C:/ti/C6xCSL/include" --include_path="C:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="c:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="/packages/ti/xdais" --include_path="/include" --include_path="c:/CCStudio_v3.1/MyProjects/Support" --include_path="C:/ti/CCStudio/c6000/dsk6713/include" --define="CHIP_6713" --display_error_number --diag_wrap=off --mem_model:data=far --preproc_with_compile --preproc_dependency="Vectors_poll.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 c6713dskinit.obj: ../c6713dskinit.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: C6000 Compiler'
-	"c:/ti/ccsv6/tools/compiler/c6000_7.4.8/bin/cl6x" -mv6710 --abi=coffabi -g --include_path="C:/ti/C6xCSL/include" --include_path="C:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="c:/ti/ccsv6/tools/compiler/c6000_7.4.8/include" --include_path="/packages/ti/xdais" --include_path="/include" --include_path="c:/CCStudio_v3.1/MyProjects/Support" --include_path="C:/ti/CCStudio/c6000/dsk6713/include" --define="CHIP_6713" --display_error_number --diag_wrap=off --mem_model:data=far --preproc_with_compile --preproc_dependency="c6713dskinit.pp" $(GEN_OPTS__FLAG) "$<"
+	"c:/ti/ccsv6/tools/compiler/c6000_7.4.12/bin/cl6x" -mv6710 --abi=coffabi -g --include_path="C:/ti/C6xCSL/include" --include_path="C:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="c:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="/packages/ti/xdais" --include_path="/include" --include_path="c:/CCStudio_v3.1/MyProjects/Support" --include_path="C:/ti/CCStudio/c6000/dsk6713/include" --define="CHIP_6713" --display_error_number --diag_wrap=off --mem_model:data=far --preproc_with_compile --preproc_dependency="c6713dskinit.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 loop_poll.obj: ../loop_poll.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: C6000 Compiler'
-	"c:/ti/ccsv6/tools/compiler/c6000_7.4.8/bin/cl6x" -mv6710 --abi=coffabi -g --include_path="C:/ti/C6xCSL/include" --include_path="C:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="c:/ti/ccsv6/tools/compiler/c6000_7.4.8/include" --include_path="/packages/ti/xdais" --include_path="/include" --include_path="c:/CCStudio_v3.1/MyProjects/Support" --include_path="C:/ti/CCStudio/c6000/dsk6713/include" --define="CHIP_6713" --display_error_number --diag_wrap=off --mem_model:data=far --preproc_with_compile --preproc_dependency="loop_poll.pp" $(GEN_OPTS__FLAG) "$<"
+	"c:/ti/ccsv6/tools/compiler/c6000_7.4.12/bin/cl6x" -mv6710 --abi=coffabi -g --include_path="C:/ti/C6xCSL/include" --include_path="C:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="c:/ti/ccsv6/tools/compiler/c6000_7.4.12/include" --include_path="/packages/ti/xdais" --include_path="/include" --include_path="c:/CCStudio_v3.1/MyProjects/Support" --include_path="C:/ti/CCStudio/c6000/dsk6713/include" --define="CHIP_6713" --display_error_number --diag_wrap=off --mem_model:data=far --preproc_with_compile --preproc_dependency="loop_poll.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
