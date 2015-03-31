65205ee85414e963e1a09786522ed8bd3d64889e
Chris Daffron <cdaffron@vols.utk.edu>
2015-03-30 22:17:58 -0400

    Started report

--- lab4/butterworth_code.m
+++ lab4/butterworth_code.m
@@ -3,30 +3,30 @@ delta_s = 0.01;
 hdelta_p = 2 * delta_p / (1 + delta_p);
 hdelta_s = delta_s / (1 / delta_s);
 
-% Wp = [0.35 0.6];
-% Ws = [0.25 0.65];
-Wp = [0.35*pi 0.6*pi];
-Ws = [0.25*pi 0.65*pi];
+Wp = [0.35 0.6];
+Ws = [0.25 0.65];
+% Wp = [0.35*pi 0.6*pi];
+% Ws = [0.25*pi 0.65*pi];
 Rp = -20 * log10(1 - hdelta_p);
 Rs = -20 * log10(hdelta_s);
 
 %% Butterworth Impulse Invariance Section
-% [n,Wn] = buttord(Wp, Ws, Rp, Rs,'s');
-% [b,a] = butter(n, Wn, 'bandpass','s');
-% freqz(b,a);
-% figure()
-% [bz, az] = impinvar( b, a );
-% %[Hz, Wz] = freqz(bz,az);
-% freqz(bz,az);
-
-%% Butterworth Bilinear Transformation Section
-[n,Wn] = buttord(Wp, Ws, Rp, Rs, 's');
-[b,a] = butter(n, Wn, 'bandpass', 's');
+[n,Wn] = buttord(Wp, Ws, Rp, Rs);
+[b,a] = butter(n, Wn, 'bandpass');
 freqz(b,a);
-figure();
-[bz, az] = bilinear( b, a, 1);
+figure()
+[bz, az] = impinvar( b, a );
 %[Hz, Wz] = freqz(bz,az);
 freqz(bz,az);
+
+%% Butterworth Bilinear Transformation Section
+% [n,Wn] = buttord(Wp, Ws, Rp, Rs, 's');
+% [b,a] = butter(n, Wn, 'bandpass', 's');
+% freqz(b,a);
+% figure();
+% [bz, az] = bilinear( b, a, 1);
+% %[Hz, Wz] = freqz(bz,az);
+% freqz(bz,az);
 
 %% Elliptic Bilinear Transformation Section
 % [n,Wn] = ellipord(Wp, Ws, Rp, Rs);
